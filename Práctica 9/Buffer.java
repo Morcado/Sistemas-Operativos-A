d1c6d08ec52da5a22541fa2a78a6829dfdd785a5
Morcado <gposcarr18@hotmail.com>
2017-10-13 12:08:01 -0500

    Mejoras

--- Práctica 9/Buffer.java
+++ Práctica 9/Buffer.java
@@ -3,6 +3,7 @@ import java.util.Stack;
 public class Buffer{
     private int cont;
     private Stack<Integer> pila;
+
     public Buffer(){
         cont = 0;
         pila = new Stack<Integer>();
@@ -18,36 +19,35 @@ public class Buffer{
         else{
             System.out.println("Buffer lleno");
         }
-        // Si el buffer está lleno, esperar a que exista
-        // por lo menos un lugar en el buffer. 
-        // Imprimir que el buffer está lleno
+        /* Si el buffer está lleno, esperar a que exista
+        por lo menos un lugar en el buffer. 
+        Imprimir que el buffer está lleno
         
-        // Insertar dato en el buffer
+        Insertar dato en el buffer
         
-        // Imprimir mensaje de inserción, la posición 
-        // donde se insertó y el dato
+        Imprimir mensaje de inserción, la posición 
+        donde se insertó y el dato*/
         
     }
     public void sacaDato(){
         if(!pila.empty()){
             int a = pila.pop();
             cont--;
-            
+
             System.out.println("El dato sacado es: " + a + " en la posicion: " + cont);
             
         }
         else{
             System.out.println("Buffer vacio");
         }
-        // Si el buffer está vacío, esperar a que
-        // exista por lo menos un dato enel buffer.
-        // Imprimir que elbuffer está vacío
+        /*Si el buffer está vacío, esperar a que
+        exista por lo menos un dato enel buffer.
+        Imprimir que elbuffer está vacío
         
-        // Sacar dato del buffer
+        Sacar dato del buffer
         
-        // Imprimir dato consumidoy la posición de 
-        // donde se sacó
-        
-    }
+        Imprimir dato consumidoy la posición de 
+        donde se sacó*/
+        }
 // Declaración de variables PRIVADAS
 }
--- Práctica 9/Principal.java
+++ Práctica 9/Principal.java
@@ -1,4 +1,8 @@
+/*Fecha: 13 de octubre de 2017
+Alumnos: Oscar Armando González Patiño, Ef*/
+
 public class Principal{
+
     public static void main (String args[]){
         Buffer b1 = new Buffer();
         Productor p = new Productor(b1);
